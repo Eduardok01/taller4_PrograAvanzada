1dc690e29f3f45246c992e98b5ce62af9e651253
Eduardok01 <e.krause01@ufromail.cl>
2024-11-15 11:38:33 -0300

    Add code post-refactor
    se agrega el codigo post refactor

--- taller4/src/main/java/org/example/Kaprekar1.java
+++ taller4/src/main/java/org/example/Kaprekar1.java
@@ -1,6 +1,12 @@
 package org.example;
 import java.util.Arrays;
 
+
+
+
+
+//de momento, el refactor del codigo es el mismo, pues
+//esta pasando de forma implicita al no hacer nada mas que return.
 public class Kaprekar1 {
 
     public static int kaprekarOp(int number) {
